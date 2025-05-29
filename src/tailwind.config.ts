b7c275a01c1b2db64e36e96b978b3eacef9b57e6
strakefoundation <73552109+straketeam@users.noreply.github.com>
2025-05-29 16:20:36 -0500

    Create tailwind.config.ts

---
+++ src/tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  content: [
+    "./client/src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+export default config
