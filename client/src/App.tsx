dea900b3e672e12512f1ecb8d7451d79891b38ef
strakefoundation <73552109+straketeam@users.noreply.github.com>
2025-05-30 07:49:49 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,43 +1,8 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { WalletProvider, useWallet } from "@/contexts/wallet-context";
-import ARKlinkoBlockchain from "@/pages/arklinko-blockchain";
-import Login from "@/pages/login";
-import Fairness from "@/pages/fairness";
-import NotFound from "@/pages/not-found";
-
-function Router() {
-  const { isAuthenticated } = useWallet();
-
-  if (!isAuthenticated) {
-    return <Login />;
-  }
-
-  return (
-    <Switch>
-      <Route path="/" component={ARKlinkoBlockchain} />
-      <Route path="/fairness" component={Fairness} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+import React from 'react'
+import ARKlinkoBlockchain from '@/pages/arklinko-blockchain'
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <WalletProvider>
-        <TooltipProvider>
-          <div className="dark">
-            <Toaster />
-            <Router />
-          </div>
-        </TooltipProvider>
-      </WalletProvider>
-    </QueryClientProvider>
-  );
+  return <ARKlinkoBlockchain />
 }
 
-export default App;
+export default App
