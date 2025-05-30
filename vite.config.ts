76f9f3e6212d695c2d4b81347a97f09d2542daa0
strakefoundation <73552109+straketeam@users.noreply.github.com>
2025-05-30 13:32:01 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { cartographer } from "@replit/vite-plugin-cartographer";
-import { runtimeErrorModal } from "@replit/vite-plugin-runtime-error-modal";
 import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    cartographer(),
-    runtimeErrorModal(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client/src"),
       "@shared": path.resolve(__dirname, "./shared"),
-      "@assets": path.resolve(__dirname, "./attached_assets"),
     },
   },
-  root: "client",
+  root: "./client",
   build: {
     outDir: "../dist/public",
     emptyOutDir: true,
