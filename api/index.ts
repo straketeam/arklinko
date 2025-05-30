5d3f898fcf34171d0f739e0b4561f67b26dd7388
strakefoundation <73552109+straketeam@users.noreply.github.com>
2025-05-30 13:42:37 -0500

    Create index.ts

---
+++ api/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { registerRoutes } from "../server/routes";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Register API routes
+registerRoutes(app);
+
+export default app;
