5809b2190bc1d0e777f938d241526c4cabfa7bdf
strakefoundation <73552109+straketeam@users.noreply.github.com>
2025-05-30 15:06:57 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,16 @@
-import { Switch, Route } from "wouter";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import ARKlinkoBlockchain from "./pages/arklinko-blockchain";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      queryFn: async ({ queryKey }) => {
-        const response = await fetch(queryKey[0] as string);
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      },
-    },
-  },
-});
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={ARKlinkoBlockchain} />
-      <Route>404 Page Not Found</Route>
-    </Switch>
-  );
-}
+import { Route, Switch } from "wouter"
+import ARKlinkoBlockchain from "./pages/arklinko-blockchain"
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router />
-    </QueryClientProvider>
-  );
+    <Switch>
+      <Route path="/" component={ARKlinkoBlockchain} />
+      <Route path="/game" component={ARKlinkoBlockchain} />
+      <Route>
+        <ARKlinkoBlockchain />
+      </Route>
+    </Switch>
+  )
 }
 
-export default App;
+export default App
